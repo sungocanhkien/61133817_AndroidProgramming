777cb14a9e49b3fb18c8e26af441a0760eefa56b
sungocanhkien <kiensu95@gmail.com>
2025-02-24 01:32:34 +0700

    Update MainActivity.java

--- Ex5_AddSubMulDiv_Anynomous/app/src/main/java/anhkien/example/ex5_addsubmuldiv_anynomous/MainActivity.java
+++ Ex5_AddSubMulDiv_Anynomous/app/src/main/java/anhkien/example/ex5_addsubmuldiv_anynomous/MainActivity.java
@@ -31,11 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         TimView();
         buttonTong.setOnClickListener(XuLyTong);
         buttonHieu.setOnClickListener(XuLyHieu);
@@ -54,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
                 String StrKQ = String.valueOf(Thuong);
                 textViewKQ.setText(StrKQ);
             }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 

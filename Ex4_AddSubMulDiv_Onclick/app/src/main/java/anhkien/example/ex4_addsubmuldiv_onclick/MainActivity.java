1347bee13b451561c6dc97058897664a06242be2
sungocanhkien <kiensu95@gmail.com>
2025-02-23 17:13:30 +0700

    update

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/anhkien/example/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/anhkien/example/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,6 +1,7 @@
 package anhkien.example.ex4_addsubmuldiv_onclick;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    void XuLyCong(View view){
+
+    }
+    void XuLyTru(View view){
+
+    }
+    void XuLyNhan(View view){
+
+    }
+    void XuLyChia(View view){
+
     }
 }

b1441d4067e73b9b41f49aa092526685acd75ae7
sungocanhkien <kiensu95@gmail.com>
2025-03-02 12:19:36 +0700

    Update MainActivity.java

--- km_to_m_App/app/src/main/java/anhkien/android/km_to_m_app/MainActivity.java
+++ km_to_m_App/app/src/main/java/anhkien/android/km_to_m_app/MainActivity.java
@@ -1,6 +1,9 @@
 package anhkien.android.km_to_m_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText1;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        button=findViewById(R.id.btn);
+        editText1=findViewById(R.id.edtkm);
     }
 }

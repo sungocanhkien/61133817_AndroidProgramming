3ecd555bff2560ecd15ce5453018399f0e6afb2e
sungocanhkien <kiensu95@gmail.com>
2025-03-08 01:24:30 +0700

    Update Home.java

--- Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/Home.java
+++ Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/Home.java
@@ -1,6 +1,9 @@
 package anhkien.android.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -21,6 +24,14 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        EditText
+        EditText editTextHome = findViewById(R.id.edtHome);
+        Button buttonHome = findViewById(R.id.btnComeback);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ComeBack = new Intent(Home.this,MainActivity.class);
+                startActivity(ComeBack);
+            }
+        });
     }
 }

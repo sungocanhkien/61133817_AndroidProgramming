a9b63b7a53a0eb9ddb2a82f9c433f0a6a3b75c9e
sungocanhkien <kiensu95@gmail.com>
2025-03-08 00:59:59 +0700

    update

--- Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package anhkien.android.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button buttonMain = findViewById(R.id.btnMainLogin);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MainLogin = new Intent(MainActivity.this,Login.class);
+                startActivity(MainLogin);
+            }
+        });
+
     }
 }

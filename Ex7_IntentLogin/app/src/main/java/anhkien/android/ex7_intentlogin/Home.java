84ca71736abc63e4f894d7fb2273ca967b5ff48e
sungocanhkien <kiensu95@gmail.com>
2025-03-08 01:21:18 +0700

    update

--- Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/Home.java
+++ Ex7_IntentLogin/app/src/main/java/anhkien/android/ex7_intentlogin/Home.java
@@ -1,6 +1,7 @@
 package anhkien.android.ex7_intentlogin;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText
     }
 }

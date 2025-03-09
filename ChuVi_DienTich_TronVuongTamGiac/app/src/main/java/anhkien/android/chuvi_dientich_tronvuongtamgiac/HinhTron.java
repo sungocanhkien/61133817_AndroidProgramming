ae7aaf12c76034c3ab49b7a09ae26ba79774b823
sungocanhkien <kiensu95@gmail.com>
2025-03-09 12:05:39 +0700

    update

---
+++ ChuVi_DienTich_TronVuongTamGiac/app/src/main/java/anhkien/android/chuvi_dientich_tronvuongtamgiac/HinhTron.java
@@ -0,0 +1,24 @@
+package anhkien.android.chuvi_dientich_tronvuongtamgiac;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HinhTron extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hinh_tron);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

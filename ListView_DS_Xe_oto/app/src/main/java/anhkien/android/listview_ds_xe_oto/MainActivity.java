43f5f009fdf85b2b260657bc4d1d1b58b8be1f22
sungocanhkien <kiensu95@gmail.com>
2025-03-12 11:11:23 +0700

    Update MainActivity.java

--- ListView_DS_Xe_oto/app/src/main/java/anhkien/android/listview_ds_xe_oto/MainActivity.java
+++ ListView_DS_Xe_oto/app/src/main/java/anhkien/android/listview_ds_xe_oto/MainActivity.java
@@ -1,6 +1,10 @@
 package anhkien.android.listview_ds_xe_oto;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button5cho = findViewById(R.id.btn5cho);
+        Button button7cho = findViewById(R.id.btn7cho);
+
+        button5cho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent oto5cho = new Intent(MainActivity.this, ListView_DS_oto.class);
+                oto5cho.putExtra("oto 5 chỗ ngồi", "Oto 5 chỗ");
+                startActivity(oto5cho);
+            }
         });
     }
 }

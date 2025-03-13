5f793773e0df8b654eb5e23c50c34bdd6cf1d6be
sungocanhkien <kiensu95@gmail.com>
2025-03-13 19:52:44 +0700

    update

--- Login/app/src/main/java/anhkien/android/login/MainActivity.java
+++ Login/app/src/main/java/anhkien/android/login/MainActivity.java
@@ -30,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_LONG).show();
                 }
             }
         });

39017aa5a204ca1316b46747791b8343edb87f93
sungocanhkien <kiensu95@gmail.com>
2025-03-15 23:36:05 +0700

    update

--- ListView_DS_Xe_oto/app/src/main/java/anhkien/android/listview_ds_xe_oto/login.java
+++ ListView_DS_Xe_oto/app/src/main/java/anhkien/android/listview_ds_xe_oto/login.java
@@ -1,6 +1,11 @@
 package anhkien.android.listview_ds_xe_oto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,21 @@ public class login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        EditText editTextusername = findViewById(R.id.myusernam);
+        EditText editTextpass = findViewById(R.id.password);
+        Button buttonbtn = findViewById(R.id.loginbtn);
+
+        buttonbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextusername.getText().toString().equals("AnhKien")&& editTextpass.getText().toString().equals("12345")){
+                    Intent intentLogin = new Intent(login.this, MainActivity.class);
+                    startActivity(intentLogin);
+                } else {
+                    Toast.makeText(login.this, "Login Failed!", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }

412cfd16223601199c9dbdab5a80bb94d885ff77
sungocanhkien <kiensu95@gmail.com>
2025-03-19 10:01:07 +0700

    Update MainActivity.java

--- ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/MainActivity.java
+++ ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/MainActivity.java
@@ -1,8 +1,11 @@
 package anhkien.android.shopgiaydep;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextPass = findViewById(R.id.edtpass);
         Button buttonLogin = findViewById(R.id.btnlogin);
 
-
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextEmail.getText().toString().equals("AnhKien") && editTextPass.getText().toString().equals(12345)){
+                    Intent intentLogin = new Intent(MainActivity.this, DS_Giay_Dep.class);
+                } else {
+                    Toast.makeText(MainActivity.this, "Email hoặc Pass đã sai! Hãy kiểm tra lại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

3ab10255934b6202052c0ba8cb73585d61c63a53
sungocanhkien <kiensu95@gmail.com>
2025-03-19 10:11:19 +0700

    update

--- ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/DS_Giay_Dep.java
+++ ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/DS_Giay_Dep.java
@@ -20,5 +20,6 @@ public class DS_Giay_Dep extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }

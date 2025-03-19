c008762a0794c9d421cb5a5ec8eaaf653502179c
sungocanhkien <kiensu95@gmail.com>
2025-03-19 09:56:54 +0700

    update

--- ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/DS_Giay_Dep.java
+++ ShopGiayDep/app/src/main/java/anhkien/android/shopgiaydep/DS_Giay_Dep.java
@@ -1,8 +1,6 @@
 package anhkien.android.shopgiaydep;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DS_Giay_Dep extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ds_giay_dep);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        EditText editTextEmail = findViewById(R.id.edtemail);
-        EditText editTextPass = findViewById(R.id.edtpass);
-        Button buttonLogin = findViewById(R.id.btnlogin);
-
-
     }
 }

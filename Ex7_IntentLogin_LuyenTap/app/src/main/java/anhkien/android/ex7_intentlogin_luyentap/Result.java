9c5c1f37c76027cd026632500833d97b67f8c96e
sungocanhkien <kiensu95@gmail.com>
2025-03-21 09:50:20 +0700

    update

--- Ex7_IntentLogin_LuyenTap/app/src/main/java/anhkien/android/ex7_intentlogin_luyentap/Result.java
+++ Ex7_IntentLogin_LuyenTap/app/src/main/java/anhkien/android/ex7_intentlogin_luyentap/Result.java
@@ -1,9 +1,6 @@
 package anhkien.android.ex7_intentlogin_luyentap;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Result extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        EditText editTextemail = findViewById(R.id.edtemail);
-        EditText editTextpass = findViewById(R.id.edtpass);
-        Button buttonlogin = findViewById(R.id.btnlogin);
-
-        buttonlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextemail.getText().toString().equals("AnhKien") && editTextpass.getText().toString().equals("123")){
-
-                }
-            }
         });
     }
 }

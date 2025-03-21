f0ac21a860ed4ba68fffcf043704748b8478e4cc
sungocanhkien <kiensu95@gmail.com>
2025-03-21 09:24:44 +0700

    Ex7_IntentLogin_LuyenTap

--- Ex7_IntentLogin_LuyenTap/app/src/main/java/anhkien/android/ex7_intentlogin_luyentap/MainActivity.java
+++ Ex7_IntentLogin_LuyenTap/app/src/main/java/anhkien/android/ex7_intentlogin_luyentap/MainActivity.java
@@ -1,7 +1,6 @@
-package anhkien.android.shopgiaydep;
+package anhkien.android.ex7_intentlogin_luyentap;
 
 import android.os.Bundle;
-import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
-public class DS_Giay_Dep extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ds_giay_dep);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ListView listViewDS_Giay_Dep = findViewById(R.id.listShop);
-        ArrayList<String> DSgiayDep = new ArrayList<>();
-        DSgiayDep.add("Dep");
-
-        Ad
-
     }
 }

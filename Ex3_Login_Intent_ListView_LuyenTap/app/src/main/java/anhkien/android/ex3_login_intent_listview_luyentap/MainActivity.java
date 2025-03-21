04e007f81c0bc524979891d01ff8dbbb9cd510b2
sungocanhkien <kiensu95@gmail.com>
2025-03-21 22:19:27 +0700

    Update MainActivity.java

--- Ex3_Login_Intent_ListView_LuyenTap/app/src/main/java/anhkien/android/ex3_login_intent_listview_luyentap/MainActivity.java
+++ Ex3_Login_Intent_ListView_LuyenTap/app/src/main/java/anhkien/android/ex3_login_intent_listview_luyentap/MainActivity.java
@@ -1,7 +1,11 @@
 package anhkien.android.ex3_login_intent_listview_luyentap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         });
         EditText editTextEmail = findViewById(R.id.edtEmail);
         EditText editTextPass = findViewById(R.id.edtPass);
-        
+        Button buttonlogin = findViewById(R.id.btnlogin);
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextEmail.getText().toString().equals("AnhKien") && editTextPass.getText().toString().equals("123")){
+                    Intent intentlogin = new Intent(MainActivity.this, PhepTong.class);
+                    startActivity(intentlogin);
+                } else {
+                    Toast.makeText(MainActivity.this, "Email hoặc Pass sai. Hãy nhập lại!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

a171bcc086fca40e2bfc3d2384d362689570fc72
sungocanhkien <kiensu95@gmail.com>
2025-03-23 22:42:51 +0700

    update

--- LuyenTap_vs_Figma/app/src/main/java/anhkien/android/luyentap_vs_figma/Home.java
+++ LuyenTap_vs_Figma/app/src/main/java/anhkien/android/luyentap_vs_figma/Home.java
@@ -1,38 +1,24 @@
 package anhkien.android.luyentap_vs_figma;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    CardView cardView;
-    @SuppressLint("MissingInflatedId")
+public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        cardView = findViewById(R.id.cardview);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentHome = new Intent(MainActivity.this, Home.class);
-                startActivity(intentHome);
-            }
         });
     }
 }

3a96af3d0c60f29b9c89c9a46de02d3096f4afbb
sungocanhkien <kiensu95@gmail.com>
2025-03-24 04:23:56 +0700

    update

--- AppLuyenTap_vs_Figma/app/src/main/java/anhkien/android/appluyentap_vs_figma/ManHinh4.java
+++ AppLuyenTap_vs_Figma/app/src/main/java/anhkien/android/appluyentap_vs_figma/ManHinh4.java
@@ -1,6 +1,8 @@
 package anhkien.android.appluyentap_vs_figma;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ManHinh4 extends AppCompatActivity {
-
+    ImageView imageViewNha4, imageViewTTheo4, imageViewSo6, imageViewSo4, imageViewSo9, imageViewSo7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,12 @@ public class ManHinh4 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imageViewNha4 = findViewById(R.id.imageHome);
+        imageViewSo6 = findViewById(R.id.imageViewSo6);
+        imageViewSo4 = findViewById(R.id.imageViewSo4);
+        imageViewSo9 = findViewById(R.id.imageViewSo9);
+        imageViewSo7 = findViewById(R.id.imageViewSo7);
+        imageViewTTheo4 = findViewById(R.id.imageTiepTheo);
+        imageViewTTheo4.setVisibility(View.GONE);
     }
 }

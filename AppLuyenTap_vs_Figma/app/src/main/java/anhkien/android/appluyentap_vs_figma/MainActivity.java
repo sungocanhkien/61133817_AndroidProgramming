f9cf9e7758d4824ee5d8af082fe0982e8f43a848
sungocanhkien <kiensu95@gmail.com>
2025-03-24 02:37:36 +0700

    Update MainActivity.java

--- AppLuyenTap_vs_Figma/app/src/main/java/anhkien/android/appluyentap_vs_figma/MainActivity.java
+++ AppLuyenTap_vs_Figma/app/src/main/java/anhkien/android/appluyentap_vs_figma/MainActivity.java
@@ -1,6 +1,9 @@
 package anhkien.android.appluyentap_vs_figma;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        imageView = findViewById(R.id.imageMau);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMau = new Intent(MainActivity.this, ManHinh2.class);
+                startActivity(intentMau);
+            }
         });
     }
 }

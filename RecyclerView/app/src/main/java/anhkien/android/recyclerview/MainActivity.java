3b43d10cdc008c94ca12f33880971ffed43076b6
sungocanhkien <kiensu95@gmail.com>
2025-03-25 22:20:36 +0700

    update

--- RecyclerView/app/src/main/java/anhkien/android/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/anhkien/android/recyclerview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }

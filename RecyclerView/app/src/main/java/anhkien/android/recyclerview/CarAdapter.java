b25cfd5fbfdc4b81990bb03a339a9744302010e6
sungocanhkien <kiensu95@gmail.com>
2025-03-25 21:42:16 +0700

    Update CarAdapter.java

--- RecyclerView/app/src/main/java/anhkien/android/recyclerview/CarAdapter.java
+++ RecyclerView/app/src/main/java/anhkien/android/recyclerview/CarAdapter.java
@@ -1,15 +1,31 @@
 package anhkien.android.recyclerview;
 
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CarAdapter extends RecyclerView.Adapter<CarAdapter> {
+import java.util.List;
+
+public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
+
+    private List<Car> danhSachXe;
+    public CarAdapter(List<Car> danhSachXe){
+        this.danhSachXe = danhSachXe;
+    }
+    
     static class CarViewHolder extends RecyclerView.ViewHolder{
         TextView tenxe;
         ImageView hinhAnhXe;
         TextView moTaXe;
-        
+
+        public CarViewHolder(@NonNull View itemView, TextView tenxe, ImageView hinhAnhXe, TextView moTaXe) {
+            super(itemView);
+            tenxe = itemView.findViewById(R.id.ten_xe);
+            hinhAnhXe = itemView.findViewById(R.id.hinh_anh_xe);
+            moTaXe = itemView.findViewById(R.id.mo_ta_xe);
+        }
     }
 }

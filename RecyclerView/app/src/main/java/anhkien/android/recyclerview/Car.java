8ff6c23f39a9d5102aab4b07684cbd915d1458b6
sungocanhkien <kiensu95@gmail.com>
2025-03-25 21:21:13 +0700

    update

--- RecyclerView/app/src/main/java/anhkien/android/recyclerview/Car.java
+++ RecyclerView/app/src/main/java/anhkien/android/recyclerview/Car.java
@@ -4,5 +4,16 @@ public class Car {
     private String ten;
     private int hinhAnhId;
     private String moTa;
-    
+
+    public String getTen() {
+        return ten;
+    }
+
+    public int getHinhAnhId() {
+        return hinhAnhId;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
 }

e55dc5916ccde3818982d3df918c359fedcba67f
sungocanhkien <kiensu95@gmail.com>
2025-03-28 20:36:08 +0700

    LuyenTap_Login_Inten_ListView_RecyclerView

---
+++ LuyenTap_Login_Intent_ListView_RecyclerView/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LuyenTap_Login_Intent_ListView_RecyclerView"
+include(":app")
+ 

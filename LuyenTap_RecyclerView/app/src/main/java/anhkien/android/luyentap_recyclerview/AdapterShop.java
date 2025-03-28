622fc47f5e1416d0a1eec3d0cb76240154e1844c
sungocanhkien <kiensu95@gmail.com>
2025-03-29 00:57:05 +0700

    Update AdapterShop.java

--- LuyenTap_RecyclerView/app/src/main/java/anhkien/android/luyentap_recyclerview/AdapterShop.java
+++ LuyenTap_RecyclerView/app/src/main/java/anhkien/android/luyentap_recyclerview/AdapterShop.java
@@ -1,13 +1,30 @@
 package anhkien.android.luyentap_recyclerview;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterShop extends RecyclerView.Adapter<>{
+public class AdapterShop extends RecyclerView.Adapter<AdapterShop.shopViewHolder>{
+
+    @NonNull
+    @Override
+    public shopViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull shopViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class shopViewHolder extends RecyclerView.ViewHolder{
         ImageView imageSanpham;

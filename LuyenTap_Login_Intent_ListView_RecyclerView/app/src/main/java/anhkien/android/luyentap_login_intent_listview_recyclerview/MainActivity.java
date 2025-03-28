797e054a1c3c962b36b31c64a4f834b0a1d69cb4
sungocanhkien <kiensu95@gmail.com>
2025-03-28 21:13:00 +0700

    update

--- LuyenTap_Login_Intent_ListView_RecyclerView/app/src/main/java/anhkien/android/luyentap_login_intent_listview_recyclerview/MainActivity.java
+++ LuyenTap_Login_Intent_ListView_RecyclerView/app/src/main/java/anhkien/android/luyentap_login_intent_listview_recyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package anhkien.android.luyentap_login_intent_listview_recyclerview;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextname, editTextpass;
+    Button buttonlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextname = findViewById(R.id.edtname);
+        editTextpass = findViewById(R.id.edtpass);
+        buttonlogin = findViewById(R.id.btnlogin);
+        
     }
 }

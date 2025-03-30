d6b911bdfd894e3e3505714176a39c3691beee19
sungocanhkien <kiensu95@gmail.com>
2025-03-30 22:32:03 +0700

    update

--- LuyenTap_TongHop_My/app/src/main/java/anhkien/android/luyentap_tonghop_my/Cau3_Car.java
+++ LuyenTap_TongHop_My/app/src/main/java/anhkien/android/luyentap_tonghop_my/Cau3_Car.java
@@ -1,4 +1,37 @@
 package anhkien.android.luyentap_tonghop_my;
 
 public class Cau3_Car {
+    private String ten;
+    private int hinhAnhId;
+    private String moTa;
+
+    public Cau3_Car(String ten, int hinhAnhId, String moTa) {
+        this.ten = ten;
+        this.hinhAnhId = hinhAnhId;
+        this.moTa = moTa;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getHinhAnhId() {
+        return hinhAnhId;
+    }
+
+    public void setHinhAnhId(int hinhAnhId) {
+        this.hinhAnhId = hinhAnhId;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
 }
--- LuyenTap_TongHop_My/app/src/main/java/anhkien/android/luyentap_tonghop_my/Cau3_CarAdapter.java
+++ LuyenTap_TongHop_My/app/src/main/java/anhkien/android/luyentap_tonghop_my/Cau3_CarAdapter.java
@@ -1,4 +1,49 @@
 package anhkien.android.luyentap_tonghop_my;
 
-public class Cau3_CarAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Cau3_CarAdapter extends RecyclerView.Adapter<Cau3_CarAdapter.Cau3_CarViewHolder> {
+
+    private List<Cau3_Car> danhSachXe;
+    public CarAdapter(List<Cau3_Car> danhSachXe){
+        this.danhSachXe = danhSachXe;
+    }
+
+    @NonNull
+    @Override
+    public Cau3_CarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Cau3_CarViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class Cau3_CarViewHolder extends RecyclerView.ViewHolder {
+        TextView tenxe;
+        ImageView hinhAnhXe;
+        TextView moTaXe;
+
+        public Cau3_CarViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tenxe = itemView.findViewById(R.id.ten_xe);
+            hinhAnhXe = itemView.findViewById(R.id.hinh_anh_xe);
+            moTaXe = itemView.findViewById(R.id.mo_ta_xe);
+        }
+    }
 }

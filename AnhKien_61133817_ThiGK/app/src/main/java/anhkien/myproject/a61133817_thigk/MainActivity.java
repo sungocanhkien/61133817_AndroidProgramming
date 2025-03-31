d7ab97c206c332fa648460c679d78fc244c4d92a
sungocanhkien <kiensu95@gmail.com>
2025-03-31 16:00:52 +0700

    update

--- AnhKien_61133817_ThiGK/app/src/main/java/anhkien/myproject/a61133817_thigk/MainActivity.java
+++ AnhKien_61133817_ThiGK/app/src/main/java/anhkien/myproject/a61133817_thigk/MainActivity.java
@@ -1,6 +1,9 @@
 package anhkien.myproject.a61133817_thigk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonCN2, buttonCN3, buttonCN4, buttonMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,38 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonCN2 = findViewById(R.id.btnCN2);
+        buttonCN3 = findViewById(R.id.btnCN3);
+        buttonCN4 = findViewById(R.id.btnCN4);
+        buttonMe = findViewById(R.id.btnMe);
+        buttonCN2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCN2 = new Intent(MainActivity.this, ChucNang2.class);
+                startActivity(intentCN2);
+            }
+        });
+        buttonCN3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCN3 = new Intent(MainActivity.this, ChucNang3.class);
+                startActivity(intentCN3);
+            }
+        });
+        buttonCN4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCN4 = new Intent(MainActivity.this, ChucNang4.class);
+                startActivity(intentCN4);
+            }
+        });
+        buttonMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMe = new Intent(MainActivity.this, AboutMe.class);
+                startActivity(intentMe);
+            }
         });
     }
 }

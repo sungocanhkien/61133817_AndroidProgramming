605dc091d154de76c90df03c8b6dd1b5958db8d8
sungocanhkien <kiensu95@gmail.com>
2025-03-31 16:59:14 +0700

    udate

--- AnhKien_61133817_ThiGK/app/src/main/java/anhkien/myproject/a61133817_thigk/ChucNang3.java
+++ AnhKien_61133817_ThiGK/app/src/main/java/anhkien/myproject/a61133817_thigk/ChucNang3.java
@@ -1,6 +1,11 @@
 package anhkien.myproject.a61133817_thigk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChucNang3 extends AppCompatActivity {
+    private ListView listView;
+    private String[] titles = {
+            "Tiến về Sài Gòn", "Giải Phóng Miền Nam", "Đất nước trọn niềm vui",
+            "Bài ca thống nhất", "Mùa Xuân trên TPHCM"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,22 @@ public class ChucNang3 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        listView = findViewById(R.id.lvCN3);
+
+        // Tạo adapter để liên kết dữ liệu với ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, titles);
+
+        listView.setAdapter(adapter);
+
+        // Bắt sự kiện khi nhấn vào một tiêu đề trong danh sách
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedTitle = titles[position];
+                Toast.makeText(ChucNang3.this, "Bạn đã chọn: " + selectedTitle, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }

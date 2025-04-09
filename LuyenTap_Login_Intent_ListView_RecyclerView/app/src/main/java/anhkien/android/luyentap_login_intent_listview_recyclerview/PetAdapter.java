f233d96f0d7ee2636c2653b4b84db6d93f348819
sungocanhkien <kiensu95@gmail.com>
2025-04-09 10:03:47 +0700

    Create PetAdapter.java

---
+++ LuyenTap_Login_Intent_ListView_RecyclerView/app/src/main/java/anhkien/android/luyentap_login_intent_listview_recyclerview/PetAdapter.java
@@ -0,0 +1,7 @@
+package anhkien.android.luyentap_login_intent_listview_recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PetAdapter extends RecyclerView.Adapter<PetAdapter.PetViewHolder> {
+    
+}

dd43957e1b553da2d62158d67029857b66aa859d
sungocanhkien <kiensu95@gmail.com>
2025-04-14 11:35:37 +0700

    Update FooterFragment.java

--- Fragment_EX3_Dynamically/app/src/main/java/anhkien/myproject/fragment_ex3_dynamically/FooterFragment.java
+++ Fragment_EX3_Dynamically/app/src/main/java/anhkien/myproject/fragment_ex3_dynamically/FooterFragment.java
@@ -3,6 +3,7 @@ package anhkien.myproject.fragment_ex3_dynamically;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,22 +29,30 @@ public class FooterFragment extends Fragment {
         Button button2 = v.findViewById(R.id.button2);
         Button button3 = v.findViewById(R.id.button3);
 
+        FragmentManager fragmentManager = getChildFragmentManager();
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment1())
+                        .commit();
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment2())
+                        .commit();
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment3())
+                        .commit();
             }
         });
 

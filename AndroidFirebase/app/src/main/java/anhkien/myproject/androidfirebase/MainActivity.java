f2be2a882457d52f187951cd24fbd5c451d7c66c
sungocanhkien <kiensu95@gmail.com>
2025-04-20 23:23:51 +0700

    Update MainActivity.java

--- AndroidFirebase/app/src/main/java/anhkien/myproject/androidfirebase/MainActivity.java
+++ AndroidFirebase/app/src/main/java/anhkien/myproject/androidfirebase/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, My Firebase!");
     }
 }

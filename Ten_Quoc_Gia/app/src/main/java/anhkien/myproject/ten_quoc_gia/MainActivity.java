39dc382bfba502697fe3654b27bafb94f51b9535
sungocanhkien <kiensu95@gmail.com>
2025-04-21 00:06:54 +0700

    Update MainActivity.java

--- Ten_Quoc_Gia/app/src/main/java/anhkien/myproject/ten_quoc_gia/MainActivity.java
+++ Ten_Quoc_Gia/app/src/main/java/anhkien/myproject/ten_quoc_gia/MainActivity.java
@@ -1,15 +1,24 @@
 package anhkien.myproject.ten_quoc_gia;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class MainActivity extends AppCompatActivity {
+    TextView textCountries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        textCountries = findViewById(R.id.txtQG);
+
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("countries");
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                StringBuilder builder = new StringBuilder();
+                for (DataSnapshot countrySnap : snapshot.getChildren()) {
+                    String countryName = countrySnap.getValue(String.class);
+                    builder.append("- ").append(countryName).append("\n");
+                }
+                textCountries.setText(builder.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Dữ liệu đọc bị lỗi!", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
